4592c34853cb0f3afe05a57f3d63782a196daa75
mdgaspar20 <54327888+mdgaspar20@users.noreply.github.com>
2019-11-05 12:16:13 -0500

    Add files via upload

---
+++ update.php
@@ -0,0 +1,218 @@
+<?php
+// Include config file
+require_once "connect.php";
+ 
+// Define variables and initialize with empty values
+$fname = $lname= $deptNum = $employeeLogin = $password = $role = "";
+$fname_e = $lname_e= $deptNum_e= $employeeLogin_e = $password_e = $role_e = "";
+$password=md5($password);
+// Processing form data when form is submitted
+if(isset($_POST["EmployeeID"]) && !empty($_POST["EmployeeID"])){
+    // Get hidden input value
+    $EmployeeID = $_POST["EmployeeID"];
+    
+    // Validate name
+      $input_fname = trim($_POST["fname"]);
+    if(empty($input_fname)){
+        $fname_e = "Please enter employee's first name.";
+    } elseif(!filter_var($input_fname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $fname_e = "Please enter a valid name.";
+    } else{
+        $fname = $input_fname;
+    }
+	     // Validate last name
+    $input_lname = trim($_POST["lname"]);
+    if(empty($input_lname)){
+        $lname_e = "Please enter employee's last name.";
+    } elseif(!filter_var($input_lname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $lname_e = "Please enter a valid name.";
+    } else{
+        $lname = $input_lname;
+    }
+    
+        // Validate user id
+    $input_login = trim($_POST["employeeLogin"]);
+    if(empty($input_login)){
+        $employeeLogin_e = "Please enter employee's login.";
+    } elseif(!filter_var($input_login, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $employeeLogin_e = "Please enter a valid userID.";
+    } else{
+        $employeeLogin = $input_login;
+    }
+  	//validate password
+	$input_password = $_POST["password"];
+	if(empty($password)){
+		$password_e="Please enter password";
+	}
+	elseif(!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/', $password)) {
+    echo "password does not meet the requirements!";
+} else{
+	$password=$input_password;
+}
+	// Validate department
+    $input_dept = trim($_POST["deptNum"]);
+    if(empty($input_dept)){
+        $deptNum_e = "Please enter a department number.";
+    } elseif(!ctype_digit($input_dept)){
+        $deptNum_e = "Please enter a positive integer value.";
+    } else{
+        $deptNum = $input_dept;
+    }
+	// Select Role
+    if(empty($_POST["role"])){
+		$role_e="Please select a role";
+	}
+	else{
+		$role=($_POST["role"]);
+	}
+    
+    // Check input errors before inserting in database
+     if(empty($fname_e) && empty($lname_e) && empty($deptNum_e) && empty($employeeLogin_e) && empty($role_e) && empty($password_e)){
+        // Prepare an update statement
+        $sql = "UPDATE employee SET Fname=?, Lname=?, DeptNum=?, EmployeeLogin=?, Password=?, Role=? WHERE EmployeeID=?";
+         
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+			mysqli_stmt_bind_param($stmt, "ssssssi", $param_fname, $param_lname, $param_deptNum, $param_login, $param_pass, $param_role, $param_id);
+            
+            // Set parameters
+           $param_fname = $fname;
+			$param_lname= $lname;
+            $param_deptNum= $deptNum;
+            $param_login= $employeeLogin;
+			$param_pass= $password;
+			$param_role= $role;
+            $param_id = $EmployeeID;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records updated successfully. Redirect to landing page
+                header("location: view.php");
+                exit();
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($conn);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["EmployeeID"]) && !empty(trim($_GET["EmployeeID"]))){
+        // Get URL parameter
+        $EmployeeID =  trim($_GET["EmployeeID"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM employee WHERE EmployeeID = ?";
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_id);
+            
+            // Set parameters
+            $param_id = $EmployeeID;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+    
+                if(mysqli_num_rows($result) == 1){
+                    /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    
+                    // Retrieve individual field value
+                    $fname = $row["Fname"];
+                    $lname = $row["Lname"];
+                    $deptNum = $row["DeptNum"];
+					$employeeLogin = $row["EmployeeLogin"];
+					$password = $row["Password"];
+					$role = $row["Role"];
+                } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+        
+        // Close connection
+        mysqli_close($conn);
+    }  else{
+        
+}
+	}
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Update Employee Records</h2>
+                    </div>
+                    <p>Please edit the input values and submit to update the record.</p>
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                       <div class="form-group <?php echo (!empty($fname_e)) ? 'has-error' : ''; ?>">
+                            <label>First Name</label>
+                            <input type="text" name="fname" class="form-control" value="<?php echo $fname; ?>">
+                            <span class="help-block"><?php echo $fname_e;?></span>
+                        </div>
+	 <div class="form-group <?php echo (!empty($lname_e)) ? 'has-error' : ''; ?>">
+                            <label>Last Name</label>
+                            <input type="text" name="lname" class="form-control" value="<?php echo $lname; ?>">
+                            <span class="help-block"><?php echo $lname_e;?></span>
+                        </div>
+    <div class="form-group <?php echo (!empty($deptNum_e)) ? 'has-error' : ''; ?>">
+                            <label>Department Number</label>
+                            <input type="text" name="deptNum" class="form-control" value="<?php echo $deptNum; ?>">
+                            <span class="help-block"><?php echo $deptNum_e;?></span>
+                        </div>
+     <div class="form-group <?php echo (!empty($employeeLogin_e)) ? 'has-error' : ''; ?>">
+                            <label>Employee Login</label>
+                            <input type="text" name="employeeLogin" class="form-control" value="<?php echo $employeeLogin; ?>">
+                            <span class="help-block"><?php echo $employeeLogin_e;?></span>
+                        </div>
+	<div class="form-group <?php echo (!empty($password_e)) ? 'has-error' : ''; ?>">
+                            <label>Password</label>
+                            <input type="password" name="password" placeholder="password" class="form-control" value="">
+                            <span class="help-block"><?php echo $password_e;?></span>
+                        </div>
+
+					    <div>
+					<label >Select Employee's Role</label>
+				<select name="role">
+			<option>---Select Role--</option>
+			<option value="employee">Employee</option>
+			<option value="manager">Manager</option>
+			<option value="admin">Admin</option>
+
+				</select>
+		                </div>
+                        <input type="hidden" name="EmployeeID" value="<?php echo $EmployeeID; ?>"/>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="view.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Employee Records</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <style type="text/css">
+        .wrapper{
+            width: 650px;
+            margin: 0 auto;
+        }
+        .page-header h2{
+            margin-top: 0;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
+</head>
+<body>
+<div class="wrapper">
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+<div class="page-header clearfix">
+<h2 class="pull-left">Employees Details</h2>
+<a href="addEmployee.php" class="btn btn-success pull-right">Add New Employee</a> 
+<a href="admin.php" class="btn btn-success pull-right">Home</a> 
+<a href="logout.php" class="btn btn-success pull-right">Logout</a>
+
+   </div>
+ <?php
+ require_once "connect.php";
+    // Attempt select query execution
+    $sql = "SELECT * FROM employee";
+      if($result = mysqli_query($conn, $sql)){
+         if(mysqli_num_rows($result) > 0){
+            echo "<table class='table table-bordered table-striped'>";
+              echo "<thead>";
+                echo "<tr>";
+                  echo "<th>Employee ID</th>";
+                  echo "<th>First Name</th>";
+                  echo "<th>Last Name</th>";
+                  echo "<th>Department No.</th>";
+                  echo "<th>Employee Login</th>";
+				  echo "<th>Action</th>";
+                  echo "</tr>";
+              echo "</thead>";
+               echo "<tbody>";
+while($row = mysqli_fetch_array($result)){
+                echo "<tr>";
+                  echo "<td>" . $row['EmployeeID'] . "</td>";
+                  echo "<td>" . $row['Fname'] . "</td>";
+                  echo "<td>" . $row['Lname'] . "</td>";
+			      echo "<td>" . $row['DeptNum'] . "</td>";
+		          echo "<td>" . $row['EmployeeLogin'] . "</td>";
+                  echo "<td>";
+    
+    echo "<a href='update.php?EmployeeID=". $row['EmployeeID'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+    echo "<a href='delete.php?EmployeeID=". $row['EmployeeID'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                echo "</td>";
+                   echo "</tr>";
+      }
+            echo "</tbody>";                            
+              echo "</table>";
+    mysqli_free_result($result);
+        } else{
+     echo "<p class='lead'><em>No records found.</em></p>";
+             }
+        } else{
+     echo "ERROR: Could not execute $sql. " . mysqli_error($conn);
+     }
+     mysqli_close($conn);
+   ?>
+</div>
+</div>        
+</div>
+</div>
+</body>
+</html>
